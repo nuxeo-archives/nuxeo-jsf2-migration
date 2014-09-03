1beaf6a0f3f8ba9bb8ea99fbfd6b7318a9bc72f5
Gildas Lefevre <glefevre@nuxeo.com>
2014-09-03 11:20:21 +0200

    NXP-14853: Clean up.

--- src/main/java/org/nuxeo/ecm/jsf2/migration/parser/ReRenderParser.java
+++ src/main/java/org/nuxeo/ecm/jsf2/migration/parser/ReRenderParser.java
@@ -32,7 +32,6 @@ import org.dom4j.tree.DefaultAttribute;
  */
 public class ReRenderParser extends GenericParser {
 
-    //private static final String REGEX_EL = "(\\#|\\$)\\{([^}]+)\\}";
     private static final String REGEX_EL = "(\\#|\\$)\\{([^}]+)\\}([^,]*)|([^,]+)";
 
     @Override
