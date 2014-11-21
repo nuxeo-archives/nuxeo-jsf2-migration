578edde9933ecbb10943f312a4488d965e8b02a8
Nelson Silva <nelson.silva@inevo.pt>
2014-11-21 10:56:02 +0000

    NXBT-857: allow recursion

--- src/main/java/org/nuxeo/ecm/jsf2/migration/MigrationToJSF2.java
+++ src/main/java/org/nuxeo/ecm/jsf2/migration/MigrationToJSF2.java
@@ -18,6 +18,12 @@ package org.nuxeo.ecm.jsf2.migration;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.List;
 
 import org.nuxeo.ecm.jsf2.migration.api.MigrationService;
@@ -33,18 +39,18 @@ public class MigrationToJSF2 {
     /**
      * @param args Path to the directory to analyze.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Check the arguments in parameter
         if (args.length == 0) {
-            System.out.println("Usage : java -jar nuxeo-jsf2-migration-<version>.jar <path to project> <auto migration> <format original files>");
+            System.out.println("Usage : java -jar nuxeo-jsf2-migration-<version>.jar <path to project> <auto migration> <format original files> <recursive>");
             return;
         }
         // Get the parameters
         String path = args[0];
-        boolean migration = args.length > 1 ? Boolean.parseBoolean(args[1])
-                : false;
-        boolean format = args.length > 2 ? Boolean.parseBoolean(args[2])
-                : false;
+        final boolean migration = args.length > 1 && Boolean.parseBoolean(args[1]);
+        final boolean format = args.length > 2 && Boolean.parseBoolean(args[2]);
+        boolean recursive = args.length > 3 && Boolean.parseBoolean(args[3]);
+
         File directory = new File(path);
 
         // Check if the directory exists
@@ -53,32 +59,61 @@ public class MigrationToJSF2 {
             return;
         }
 
+        if (!recursive) {
+            if (!isValidProjectDirectory(path)) {
+                System.out.println("The specified directory is not a valid project directory.");
+                return;
+            }
+            processDirectory(directory.getAbsolutePath(), migration, format);
+        } else {
+            Path startingDir = Paths.get(path);
+            Files.walkFileTree(startingDir, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir,
+                    BasicFileAttributes attrs) throws IOException {
+                    return processDirectory(dir.toFile().getPath(),
+                        migration, format) ?
+                        FileVisitResult.SKIP_SUBTREE : FileVisitResult.CONTINUE;
+                }
+            });
+        }
+    }
+
+    private static boolean processDirectory(String directory, boolean migration, boolean format) {
+        if (!isValidProjectDirectory(directory)) {
+            return false;
+        }
+        System.out.println(String.format("Analyzing %s", directory));
         long start = System.currentTimeMillis();
         MigrationService migrationService = new MigrationServiceImpl();
 
-        // Check that a proper directory has been defined
-        String pathXhtmlRootDirectory = String.format(
-                "%s/src/main/resources/web/nuxeo.war/",
-                directory.getAbsolutePath());
-        File xhtmlRootDirectory = new File(pathXhtmlRootDirectory);
-        if (!xhtmlRootDirectory.exists()) {
-            System.out.println("The specified directory is not a valid project directory.");
-        }
-
         // Parse all files in the project directory to get all the XHTML files
-        List<File> listXHTMLFiles = migrationService.getAllXhtmlFiles(xhtmlRootDirectory);
+        List<File> listXHTMLFiles = migrationService.getAllXhtmlFiles(getXHTMLRootDirectory(directory));
         // Generate the report
-        File report = new File(path + "/report.txt");
+        File report = new File(directory + "/report.txt");
         try {
             migrationService.analyzeProject(report, listXHTMLFiles, migration,
-                    format);
+                format);
         } catch (IOException ex) {
             System.out.println(String.format(
-                    "Error while generating the report : %s", ex.getMessage()));
+                "Error while generating the report : %s", ex.getMessage()));
         }
 
         long timeElapsed = System.currentTimeMillis() - start;
-        System.out.println(String.format("The analyze is done in %d ms",
-                Long.valueOf(timeElapsed)));
+        System.out.println(String.format("The analyze is done in %d ms", timeElapsed));
+
+        return true;
+    }
+
+    private static File getXHTMLRootDirectory(String directory) {
+        String pathXhtmlRootDirectory = String.format(
+            "%s/src/main/resources/web/nuxeo.war/",
+            directory);
+        return new File(pathXhtmlRootDirectory);
+    }
+
+    private static boolean isValidProjectDirectory(String directory) {
+        File xhtmlRootDirectory = getXHTMLRootDirectory(directory);
+        return xhtmlRootDirectory.exists();
     }
 }
